3149e1e9a545d96f226cb12659c2e962b602d898
Nazar Yanovets <nyanovet8@gmail.com>
2024-03-03 14:25:04 +0200

    create caffeine cache

--- src/main/scala/forex/services/rates/interpreters/OneFrame.scala
+++ src/main/scala/forex/services/rates/interpreters/OneFrame.scala
@@ -1,7 +1,9 @@
 package forex.services.rates.interpreters
 
-import cats.effect.{ConcurrentEffect, Resource}
-import cats.implicits.{catsSyntaxApplicativeError, catsSyntaxEitherId, toFunctorOps}
+import cats.Applicative
+import cats.effect.{ ConcurrentEffect, Resource }
+import cats.implicits.{ catsSyntaxApplicativeError, catsSyntaxEitherId, toFunctorOps }
+import com.github.benmanes.caffeine.cache.{ Cache, Caffeine }
 import forex.config.OneFrameConfig
 import forex.domain.Rate
 import forex.http.rates.Protocol.responseDecoder
@@ -14,19 +16,31 @@ import org.http4s.client.Client
 import org.http4s.blaze.client.BlazeClientBuilder
 import org.typelevel.ci.CIString
 
+import java.time.Duration
 import scala.concurrent.ExecutionContext
 
 class OneFrame[F[_]: ConcurrentEffect](config: OneFrameConfig) extends Algebra[F] {
-  private val blazeClient : Resource[F, Client[F]] = BlazeClientBuilder[F](ExecutionContext.global).resource
+  private val blazeClient: Resource[F, Client[F]]                = BlazeClientBuilder[F](ExecutionContext.global).resource
   private implicit val rateDecoder: EntityDecoder[F, List[Rate]] = jsonOf[F, List[Rate]]
 
-  override def get(pair: Rate.Pair): F[Error Either Rate] = {
-    buildRequest(pair)
-      .map {
-        case Right(value) => Right(value.last)
-        case Left(value) => Left(value)
-      }
-  }
+  private val cache: Cache[Rate.Pair, List[Rate]] = Caffeine
+    .newBuilder()
+    .expireAfterWrite(Duration.ofSeconds(config.ttl.toSeconds))
+    .build[Rate.Pair, List[Rate]]()
+
+  override def get(pair: Rate.Pair): F[Error Either Rate] =
+    Option(cache.getIfPresent(pair)) match {
+      case Some(v) =>
+        Applicative[F].pure(Right(v.last))
+      case None =>
+        buildRequest(pair).map {
+          case Right(value) =>
+            cache.put(pair, value)
+            Right(value.last)
+          case Left(error) =>
+            Left(error)
+        }
+    }
 
   def buildRequest(pair: Rate.Pair): F[Error Either List[Rate]] = {
     val uri = s"${config.url}/rates"
@@ -37,8 +51,8 @@ class OneFrame[F[_]: ConcurrentEffect](config: OneFrameConfig) extends Algebra[F
       headers = Headers(Header.Raw(CIString("token"), config.token))
     )
 
-    blazeClient.use(client =>
-        client.expect[List[Rate]](request))
+    blazeClient
+      .use(client => client.expect[List[Rate]](request))
       .map(response => response.asRight[Error])
       .handleError(_ => OneFrameLookupFailed("Response Error").asLeft[List[Rate]])
   }
